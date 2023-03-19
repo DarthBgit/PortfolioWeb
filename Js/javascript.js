652dd598ab5a0dc218a143af5a60b7fb092fc4bd
sergio <sergiovencillon10@gmail.com>
2023-03-19 17:26:03 +0100

    update_19-03-23

--- Js/javascript.js
+++ Js/javascript.js
@@ -42,8 +42,8 @@ function showSlides(n){
 
   /* CAMBIO AUTOMATICO DE IMAGENES*/
   
-  setInterval(function time(){
-  showSlides(index += 1)},12000)
+  // setInterval(function time(){
+  // showSlides(index += 1)},12000)
 
 
   // MENU HEADER OCULTO
